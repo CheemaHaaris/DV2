8828d3569d6a3286824d9dccc8a290225bd9719d
Haaris Afzal Cheema <Cheema_Haaris@student.ceu.edu>
2022-01-17 02:12:27 +0100

    Finalizing Rmd

--- DV2_HW1_HaarisCheema.R
+++ DV2_HW1_HaarisCheema.R
@@ -3,6 +3,8 @@ library(data.table)
 library(viridis)
 library(rnaturalearth)
 library(rnaturalearthdata)
+library(lubridate)
+library(tidyverse)
 
 # 0) Load the nycflights13 package and check what kind of datasets exist in the package,
 # then create a copy of flights dataset into a data.table object, called flight_data.
@@ -11,13 +13,13 @@ library(rnaturalearthdata)
 
 library(nycflights13)
 
-data(package = 'nycflights13')
-
-?nycflights13::flights
+# checking the datasets in the package
+# data(package = 'nycflights13')
 
 flight_data <- data.table(nycflights13::flights)
 
-str(flight_data)
+# checking the variable structure
+# str(flight_data)
 
 # 1) Which destination had the lowest avg arrival delay from LGA with minimum 100 flight to that destination?
 
@@ -47,13 +49,13 @@ merged_data_2[, .(sum_of_distance = sum(distance)), by = name][which.max(sum_of_
 
 # 5) Plot the monthly number of flights with 20+ mins arrival delay!
 
-flight_data$date <- substr(flight_data$time_hour,1,10)
+flight_data$date <- as.Date(format(flight_data$time_hour,'%Y-%m-01'))
 
-ggplot(flight_data[arr_delay > 20,.N, by = month], aes( x = month, y = N)) + geom_bar(aes(fill = N), stat = 'identity') +
+ggplot(flight_data[arr_delay > 20,.N, by = date], aes( x = date, y = N)) + geom_bar(aes(fill = N), stat = 'identity') +
   labs( x = 'Date', y = 'Number of Flights') + theme_bw() +
   scale_fill_gradient(low = "dodgerblue4", high = "firebrick2")
 
-#### Issue - change x axis labels
+
   
 # 6) Plot the departure delay of flights going to IAH and the related day's wind speed on a scatterplot!
 # Is there any association between the two variables? Try adding a linear model.
@@ -74,7 +76,7 @@ joined <- merge(delays, daily_wind_speed, by = c('month','day','origin'))
 
 ggplot(joined, aes(x = dep_delay, y = mean_wind_speed, col = mean_wind_speed)) +
   geom_point() +
-  geom_smooth( method= 'lm' ) +
+  geom_smooth( method= 'lm' , formula = 'y~x') +
   labs(x = 'Departure delay' , y = 'Mean of wind speed', title = 'Association between departure delay and mean wind speed') +
   theme_bw() +
   scale_colour_gradient(low = "dodgerblue4", high = "firebrick2")
@@ -91,15 +93,24 @@ dest <- flight_data[, .(flights = .N), by = dest]
 
 merged_data_3 <- merge(dest, airports, by.x = 'dest', by.y = 'faa')
 
-world <- ne_countries(scale = "medium", returnclass = "sf")
+
+world <- map_data("world")
 
 ggplot() +
-  geom_sf(data = world) +
+  geom_map(
+    data = world, map = world,
+    aes(long, lat, map_id = region),
+    color = "black", fill = "lightgray", size = 0.1
+  )  +
   geom_point(data = merged_data_3, mapping = aes(x = lon, y = lat, size = flights, colour = flights)) + 
   coord_sf() + theme_bw() + labs( x = 'longitude', y = 'latitude', title = 'Airports and the number of flights arriving at them') +
   theme(legend.position="top",
-        legend.text = element_text(size = 7, face = "bold"),
+        legend.text = element_text(size = 7),
         axis.title.x = element_blank(),
         axis.title.y = element_blank()) +
-  scale_colour_gradient(low = "dodgerblue4", high = "firebrick2")
+  scale_colour_gradient(low = "dodgerblue4", high = "firebrick2") +
+  scale_x_continuous(breaks=seq(-100, 200, by = 100))
+
+
+
 
